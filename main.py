7b2603fdebd4851000da277306d9df7d44237f32
yngarn <yngve@IP5CG42544TL>
2025-05-10 14:24:02 +0200

    updated main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("greetings boots")
+def get_book_text(filepath):    #hent boktekst fra filsti
+    with open(filepath) as f:   # gjør noe med f (filen) fra filsti her
+        file_contents = f.read()    # Les filen
+    return file_contents    #Returner/hent filen
+
+def main():
+    print(get_book_text("books/frankenstein.txt"))  #Print/vis teksten fra filen
+main()
