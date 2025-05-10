9ea3430240ac41091d3a5eaa9b9a2d5701e569c7
yngarnHP <yngve@IP5CG42544TL>
2025-05-10 16:22:34 +0200

    main.py + stats.py

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 def main():
-    book_path = "books/frankenstein.txt"    # filsti
-    text = get_book_text(book_path)         # hente test fra filsti
-    print(text)                             #Printe/vis text fra fil/bok
+    book_path = "books/frankenstein.txt"            # Filsti
+    text = get_book_text(book_path)                 # Hente test fra filsti
+    num_words = get_num_words(text)                 # Teller ordene
+    print(f"{num_words} words found in the document")
+
 
 def get_book_text(path):    # Finn bok fra filsti
     with open(path) as f:   # Gjør noe med f (filen) fra filsti her
         return f.read()     # Les filen
     
+
+from stats import get_num_words
+
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_num_words(text):
+    words = text.split()    # Deler opp ordene i setningene
+    return len(words)
